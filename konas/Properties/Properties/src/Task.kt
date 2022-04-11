3156beff3b342f418979c1723c323412a662e018
unluckyjung <ybook2012@gmail.com>
2022-04-12 02:03:57 +0900

    feat: set을 이용한 값 변경 + 다른 프로퍼티 변경 구현

--- konas/Properties/Properties/src/Task.kt
+++ konas/Properties/Properties/src/Task.kt
@@ -1,5 +1,27 @@
-class PropertyExample() {
+class PropertyExample {
     var counter = 0
-    var propertyWithCounter: Int? = null
-        set
+    var propertyWithCounter: Int = 0
+        set(value) {
+            field = value
+            counter++
+        }
+}
+
+class Member(
+    private val name: String
+) {
+    val otherName: String
+        get() = "$name and goodall"
+
+    var number = 0
+        set(value) {
+            field = value * 10
+        }
+}
+
+fun main() {
+    val member = Member("jys")
+    println(member.otherName) // "jys and goodall"
+    member.number = 20
+    print(member.number)    //  200
 }
