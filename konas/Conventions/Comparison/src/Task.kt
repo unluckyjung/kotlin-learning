ff2ef91776380e1716f636967532b436d020a1d5
unluckyjung <ybook2012@gmail.com>
2022-04-11 23:33:43 +0900

    feat: when을 이용한 코틀린 스타일의 Comparable 인터페이스 구현

--- konas/Conventions/Comparison/src/Task.kt
+++ konas/Conventions/Comparison/src/Task.kt
@@ -1,5 +1,20 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    //    override fun compareTo(other: MyDate): Int {
+//        // java style
+//        if(year == other.year){
+//            if(month == other.month){
+//                return dayOfMonth.compareTo(other.dayOfMonth)
+//            }
+//            return month.compareTo(other.month)
+//        }
+//        return year.compareTo(other.year)
+//    }
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year.compareTo(other.year)
+        month != other.month -> month.compareTo(other.month)
+        else -> dayOfMonth.compareTo(other.dayOfMonth)
+    }
+
 }
 
 fun test(date1: MyDate, date2: MyDate) {
