087afda0e283712d0771d2cbf5860591f820d1c1
unluckyjung <ybook2012@gmail.com>
2022-11-06 00:04:47 +0900

    test: llist 형태로 비교하는 예시 추가

--- kotlin-learning/src/test/kotlin/basic/kotest/RecursiveCompareTest.kt
+++ kotlin-learning/src/test/kotlin/basic/kotest/RecursiveCompareTest.kt
@@ -18,8 +18,15 @@ class RecursiveCompareTest {
         val privateMember3 = PrivateMember(age = 2, name = "yoonsung", nickName = "goodall")
         privateMember1 shouldBeEqualToComparingFields privateMember3  // pass
 
+
         // shouldNot, private 도 같이 확인하게 할 수 있다.
         privateMember1.shouldNotBeEqualToComparingFields(privateMember3, ignorePrivateFields = false)
+
+        // nickName 을 제외하고 비교할 수 있다.
+        privateMember1.shouldBeEqualToComparingFieldsExcept(
+            PrivateMember(age = 1, name = "jys", nickName = "fortune"),
+            ignoreProperty = PrivateMember::nickName
+        )
     }
 
     @DisplayName("shouldBeEqualToUsingFields Test")
@@ -37,11 +44,11 @@ class RecursiveCompareTest {
         member1.shouldBeEqualToUsingFields(member3, Member::age, Member::nickName)
 
         val member4 = Member(age = 1, name = "jys", nickName = "fortune")
-        // nickName 이 다른지 비교한다.
+        // nickName 이 달라서 !=
         member1.shouldNotBeEqualToUsingFields(member4, Member::nickName)
     }
 
-    @DisplayName("shouldBeEqualToIgnoringFields [가장 많이 쓸것 같은 매칭비교 함수]")
+    @DisplayName("shouldBeEqualToIgnoringFields")
     @Test
     fun recursiveCompareTest3() {
 
@@ -54,10 +61,34 @@ class RecursiveCompareTest {
         // age, nickName 을 제외한 필드를 비교한다.
         member1.shouldBeEqualToIgnoringFields(member2, Member::age, Member::nickName)
     }
+
+    @DisplayName("List shouldBeEqualToComparingFields 비교")
+    @Test
+    fun listRecursiveCompareTest4() {
+
+        val member1 = Member(age = 1, name = "jys", nickName = "goodall")
+        val member2 = Member(age = 2, name = "jys", nickName = "fortune")
+
+        val team1 = Team(listOf(member1, member2))
+        val team2 = Team(listOf(member1, member2))
+
+        team1.shouldBeEqualToComparingFields(team2) // ==
+
+        // member2 가 없음.
+        team1.shouldNotBeEqualToComparingFields(Team(listOf(member1)))  // !=
+
+        val member3 = Member(age = 3, name = "jys", nickName = "fortune")
+        // 다른 age 를 가진 멤버가 들어감.
+        team1.shouldNotBeEqualToComparingFields(Team(listOf(member1, member3))) // !=
+
+        // 다른 객체이지만 값은 완전히 동등, 하지만 객체 해시값이 달라서 != (필드내부값들로 확인하는것이 아님)
+        val member4 = Member(age = 2, name = "jys", nickName = "fortune")
+        team1.shouldNotBeEqualToComparingFields(Team(listOf(member1, member4))) // !=
+    }
 }
 
 class Team(
-    private val privateMembers: List<Member>
+    val members: List<Member>
 )
 
 class PrivateMember(
