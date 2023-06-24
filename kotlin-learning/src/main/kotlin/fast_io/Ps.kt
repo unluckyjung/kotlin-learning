2bc7e8400d25e26bd32524fa536c7a7cc1b8930d
unluckyjung <ybook2012@gmail.com>
2023-06-25 01:00:04 +0900

    feat: kotlin fast  io 샘플 정리

---
+++ kotlin-learning/src/main/kotlin/fast_io/Ps.kt
@@ -0,0 +1,55 @@
+package fast_io
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() {
+    val count = readln().toInt()
+    val sb = StringBuilder()
+
+    repeat(count) {
+        val st = StringTokenizer(readln())
+        val n = st.nextToken().toInt()
+        val m = st.nextToken().toInt()
+        sb.append(n + m).append("\n")
+    }
+    println(sb)
+}
+
+private fun useStringTokenizer() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val count = br.readLine().toInt()
+    val sb = StringBuilder()
+    repeat(count) {
+        // StringTokenizer 은 정규식처리하는 Split 보다 조금 더 빠르긴함.
+        val st = StringTokenizer(br.readLine())
+        val n = st.nextToken().toInt()
+        val m = st.nextToken().toInt()
+        sb.append(n + m).append("\n")
+    }
+    println(sb)
+}
+
+private fun useBufferedReader() {
+    // BufferedReader 가 readLine 보다 조금더 빠르긴 함. 크게 차이없음.
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val count = br.readLine().toInt()
+    val sb = StringBuilder()
+    repeat(count) {
+        val (n, m) = br.readLine().split(" ").map { it.toInt() }
+        sb.append(n + m).append("\n")
+    }
+    println(sb)
+}
+
+private fun use_readln() {
+    // readln 은 내부적으로 readLine 을 사용
+    val count = readln().toInt()
+    val sb = StringBuilder()
+    repeat(count) {
+        val (n, m) = readln().split(" ").map { it.toInt() }
+        sb.append(n + m).append("\n")
+    }
+    println(sb)
+}
